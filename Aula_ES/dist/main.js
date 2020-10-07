a48afab77b3cfd21289b9a845e5363406b0ccf94
Jemison Vidal <jemison.santoos@gmail.com>
2020-10-07 17:43:17 -0300

    challenge1

---
+++ Aula_ES/dist/main.js
@@ -0,0 +1,135 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/@babel/runtime/helpers/classCallCheck.js":
+/*!***************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/classCallCheck.js ***!
+  \***************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function _classCallCheck(instance, Constructor) {\n  if (!(instance instanceof Constructor)) {\n    throw new TypeError(\"Cannot call a class as a function\");\n  }\n}\n\nmodule.exports = _classCallCheck;\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/classCallCheck.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/createClass.js":
+/*!************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/createClass.js ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function _defineProperties(target, props) {\n  for (var i = 0; i < props.length; i++) {\n    var descriptor = props[i];\n    descriptor.enumerable = descriptor.enumerable || false;\n    descriptor.configurable = true;\n    if (\"value\" in descriptor) descriptor.writable = true;\n    Object.defineProperty(target, descriptor.key, descriptor);\n  }\n}\n\nfunction _createClass(Constructor, protoProps, staticProps) {\n  if (protoProps) _defineProperties(Constructor.prototype, protoProps);\n  if (staticProps) _defineProperties(Constructor, staticProps);\n  return Constructor;\n}\n\nmodule.exports = _createClass;\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/createClass.js?");
+
+/***/ }),
+
+/***/ "./src/components/Player/CustomPlayer.js":
+/*!***********************************************!*\
+  !*** ./src/components/Player/CustomPlayer.js ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return CustomPlayer; });\n/* harmony import */ var _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/classCallCheck */ \"./node_modules/@babel/runtime/helpers/classCallCheck.js\");\n/* harmony import */ var _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/createClass */ \"./node_modules/@babel/runtime/helpers/createClass.js\");\n/* harmony import */ var _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\nvar CustomPlayer = /*#__PURE__*/function () {\n  function CustomPlayer(width, height, message) {\n    _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0___default()(this, CustomPlayer);\n\n    this.width = width;\n    this.height = height;\n    this.message = message;\n    this.videoElementHTML = document.createElement(\"video\");\n    this.videoElementHTML.setAttribute(\"width\", this.width);\n    this.videoElementHTML.setAttribute(\"height\", this.height);\n    this.videoElementHTML.innerHTML = this.message;\n  }\n\n  _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1___default()(CustomPlayer, [{\n    key: \"setSources\",\n    value: function setSources(sources) {\n      var _this = this;\n\n      sources.map(function (source) {\n        var sourceElement = document.createElement(\"source\");\n        sourceElement.setAttribute(\"src\", source.src);\n        sourceElement.setAttribute(\"type\", source.type);\n\n        _this.videoElementHTML.appendChild(sourceElement);\n      });\n    }\n  }, {\n    key: \"render\",\n    value: function render(containerHTML) {\n      var containerElement = document.getElementById(containerHTML);\n\n      if (!containerElement) {\n        var sectionElement = document.createElement(\"section\");\n        sectionElement.setAttribute(\"id\", containerHTML);\n        sectionElement.appendChild(this.videoElementHTML);\n        document.body.appendChild(sectionElement);\n      } else {\n        containerHTML.appendChild(this.videoElementHTML);\n      }\n    }\n  }]);\n\n  return CustomPlayer;\n}();\n\n\n\n//# sourceURL=webpack:///./src/components/Player/CustomPlayer.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_Player_CustomPlayer__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/Player/CustomPlayer */ \"./src/components/Player/CustomPlayer.js\");\n\nvar video = new _components_Player_CustomPlayer__WEBPACK_IMPORTED_MODULE_0__[\"default\"](800, 600, \"Your browser does not support the video tag.\");\nvideo.setSources([{\n  src: \"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4\",\n  type: \"video/mp4\"\n}, {\n  src: \"I_didnt_find_the_ogg_extension_link.ogg\",\n  type: \"video/ogg\"\n}]);\nvideo.render(\"MyPlayerId\");\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
---
+++ Aula_ES/src/components/Player/CustomPlayer.js
@@ -0,0 +1,33 @@
+export default class CustomPlayer {
+  constructor(width, height, message) {
+    this.width = width;
+    this.height = height;
+    this.message = message;
+
+    this.videoElementHTML = document.createElement("video");
+    this.videoElementHTML.setAttribute("width", this.width)
+    this.videoElementHTML.setAttribute("height", this.height)
+    this.videoElementHTML.innerHTML = this.message;
+  }
+
+  setSources(sources) {
+    sources.map(source => {
+      const sourceElement = document.createElement("source");
+      sourceElement.setAttribute("src", source.src)
+      sourceElement.setAttribute("type", source.type)
+      this.videoElementHTML.appendChild(sourceElement)
+    })
+  }
+
+  render(containerHTML) {
+    const containerElement = document.getElementById(containerHTML);
+    if (!containerElement) {
+      const sectionElement = document.createElement("section");
+      sectionElement.setAttribute("id", containerHTML)
+      sectionElement.appendChild(this.videoElementHTML);
+      document.body.appendChild(sectionElement);
+    } else {
+      containerHTML.appendChild(this.videoElementHTML);
+    }
+  }
+}
